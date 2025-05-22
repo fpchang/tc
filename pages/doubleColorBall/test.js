9431862d9c1e254aac335234e6fd0bc8dabf9d09
fpchang <hanfengcfp@outlook.com>
2025-05-22 22:34:32 +0800

    123

--- pages/doubleColorBall/test.js
+++ pages/doubleColorBall/test.js
@@ -1,56 +1,21 @@
-<template>
-  <view>
-    <view>
+const redball=[],blueball=[];
+let i = 1;
+while (i < 34) {
+    redball.push(i);
+    if(i<17){
+        blueball.push(i)
+    }
+    i++;
+}
 
-		<button @click="calculate">开始计算</button>
-	</view>
-	<view> 
-		<view>
-			<text v-for="item of next.red">
-				<uni-badge class="uni-badge-left-margin" :text="item" size="50"/>
-			</text>
-			<text>
-				<uni-badge class="uni-badge-left-margin" type="primary" :text="next.blue" size="50"/>
-			</text>
-		</view>
-		
-	</view>
-  </view>
-</template>
-
-<script>
-import { ref, computed } from "vue";
-export default {
-  setup() {
-    const redball = computed(() => {
-      const arr = [];
-      let i = 1;
-      while (i < 34) {
-        arr.push(i);
-        i++;
-      }
-      return arr;
-    });
-    const blueball = computed(() => {
-      const arr = [];
-      let i = 1;
-      while (i < 17) {
-        arr.push(i);
-        i++;
-      }
-      return arr;
-    });
-	let next=ref({red:[],blue:1});
-    return { redball, blueball,next };
-  },
-  data() {
-    return {};
-  },
-  methods: {
+let next={red:[],blue:1};
+let fnObj={
+      
+	
     calculate() {
       console.log("calculate");
      
-		uni.showLoading();
+		console.log("开始计算")
       let history = [
 		// { id:2025041, red:[6, 10, 17, 19, 25, 31], blue: 6 },
 		// { id:2025042, red:[7, 13, 19, 23, 27, 33], blue: 12 },
@@ -73,17 +38,17 @@ export default {
         return { red: item.red, blue: item.blue };
       });
      
-      let target = this.getTarget(history.length+1);
+      let target = fnObj.getTarget(history.length+1);
 	  
 	   
 	  let i =0;
-	   this.next = target[target.length-1];
+        next = target[target.length-1];
 	  target.splice(target.length-1,1);
 	  let time = Date.now();
      // while(JSON.stringify(target)!=JSON.stringify(hisoryFormat)&&i<3100000){
-		while(!this.isequqlBall(hisoryFormat,target)&&i<4000000){
-		target=this.getTarget(history.length+1);
-		this.next = target[target.length-1];
+		while(!fnObj.isequqlBall(hisoryFormat,target)&&i<4000000){
+		target=fnObj.getTarget(history.length+1);
+		next = target[target.length-1];
 		 target.splice(target.length-1,1);
 		 if(i%1000000==0){
 			 console.log("100万：：",i,"time",Date.now()-time);
@@ -93,14 +58,14 @@ export default {
 		i++;
 		     
       }
-	  console.log("循环结束",target,i);
-	  uni.hideLoading()
+	  console.log("循环结束",target,i,blueball);
+	 // uni.hideLoading()
 	 
     },
 	isequqlBall(a=[],b=[]){
 		let isequal=true;
 		for(let i =0;i<a.length;i++){
-			if(!this.isEqualArray(a[i].red,b[i].red) || a[i].blue!=b[i].blue){
+			if(!fnObj.isEqualArray(a[i].red,b[i].red) || a[i].blue!=b[i].blue){
 				isequal=false;
 				break; 
 			}
@@ -121,8 +86,8 @@ export default {
 	getTarget(len){
 		let compareArr=[];
 		for (let i = 0; i <len; i++) {
-        let redBallSelect = this.getRandomRedBall();
-        let blueBallSelect = this.getRandomBlue();
+        let redBallSelect = fnObj.getRandomRedBall();
+        let blueBallSelect = fnObj.getRandomBlue();
         compareArr.push({ red: redBallSelect, blue: blueBallSelect });
       }
       return compareArr;
@@ -131,21 +96,18 @@ export default {
       return arr[Math.floor(Math.random() * arr.length)];
     },
     getRandomBlue() {
-      return this.getRandomFromArray(this.blueball);
+      return fnObj.getRandomFromArray(blueball);
     },
     getRandomRedBall() {
-      let targetArr = [...this.redball];
+      let targetArr = [...redball];
       let redBallSelect = [];
       while (redBallSelect.length < 6) {
-        let r = this.getRandomFromArray(targetArr);
+        let r = fnObj.getRandomFromArray(targetArr);
         let index = targetArr.indexOf(r);
         redBallSelect.push(r);
         targetArr.splice(index, 1);
       }
       return redBallSelect.sort((a,b)=>{return a-b});
     },
-  },
-};
-</script>
-
-<style></style>
+  }
+  fnObj.calculate();

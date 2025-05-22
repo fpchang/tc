536260925a055d219b06e26cd756c028b84e5b8b
fpchang <hanfengcfp@outlook.com>
2025-05-22 15:47:51 +0800

    11

---
+++ pages/doubleColorBall/doubleColorBall.vue
@@ -0,0 +1,124 @@
+<template>
+  <view>
+    <view>
+
+		<button @click="calculate">开始计算</button>
+	</view>
+	<view> 
+		<view>
+			<text v-for="item of next.red">
+				<uni-badge class="uni-badge-left-margin" :text="item" size="50"/>
+			</text>
+			<text>
+				<uni-badge class="uni-badge-left-margin" type="primary" :text="next.blue" size="50"/>
+			</text>
+		</view>
+		
+	</view>
+  </view>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const redball = computed(() => {
+      const arr = [];
+      let i = 1;
+      while (i < 34) {
+        arr.push(i);
+        i++;
+      }
+      return arr;
+    });
+    const blueball = computed(() => {
+      const arr = [];
+      let i = 1;
+      while (i < 17) {
+        arr.push(i);
+        i++;
+      }
+      return arr;
+    });
+	let next=ref({red:[],blue:1});
+    return { redball, blueball,next };
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    calculate() {
+      console.log("calculate");
+     
+		uni.showLoading();
+      let history = [
+		{ id:2025041, red:[6, 10, 17, 19, 25, 31], blue: 6 },
+		{ id:2025042, red:[7, 13, 19, 23, 27, 33], blue: 12 },
+		{ id:2025043, red:[3, 12, 16, 19, 20, 32], blue: 13 },
+		{ id:2025044, red:[2, 3, 7, 8, 14, 28], blue: 11},
+		{ id:2025045, red:[2, 14, 19, 24, 29, 33], blue: 13 },
+		{ id:2025046, red:[2, 3, 10, 16, 29, 33], blue: 1 },
+        { id:2025047, red:[1, 14, 21, 22, 23, 31], blue: 12 },
+        { id:2025048, red:[16, 21, 22, 23, 27, 31], blue: 10 },
+		{ id:2025049, red:[3, 6, 19, 27, 29, 33], blue: 11 },
+		{ id:2025050, red:[9, 12, 15, 18, 22, 33], blue: 16 },
+		{ id:2025051, red:[1, 2, 3, 4, 17, 22], blue: 1 },
+		{ id:2025052, red:[6, 7, 10, 17, 20, 26], blue: 9 },
+		{ id:2025053, red:[6, 9, 10, 13, 30, 33], blue: 7 },
+		{ id:2025054, red:[5, 7, 10, 21, 24, 27], blue: 16 },
+		{ id:2025055, red:[2, 5, 22, 27, 29, 33], blue: 12 },
+		{ id:2025056, red:[1, 2, 10, 14, 28, 31], blue: 3 }
+      ];
+      let hisoryFormat = history.map((item) => {
+        return { red: item.red, blue: item.blue };
+      });
+     
+      let target = this.getTarget(history.length+1);
+	  
+	   
+	  let i =0;
+	   this.next = target[target.length-1];
+	  target.splice(target.length-1,1);
+      while(JSON.stringify(target)!=JSON.stringify(hisoryFormat)&&i<91000000){
+		target=this.getTarget(history.length+1);
+		this.next = target[target.length-1];
+		 target.splice(target.length-1,1);
+		 //console.log("ttt",target)
+		i++;
+		     
+      }
+	  console.log("循环结束",target,i);
+	  uni.hideLoading()
+	 
+    },
+	getTarget(len){
+		let compareArr=[];
+		for (let i = 0; i <len; i++) {
+        let redBallSelect = this.getRandomRedBall();
+        let blueBallSelect = this.getRandomBlue();
+        compareArr.push({ red: redBallSelect, blue: blueBallSelect });
+      }
+      return compareArr;
+	},
+    getRandomFromArray(arr = []) {
+      return arr[Math.floor(Math.random() * arr.length)];
+    },
+    getRandomBlue() {
+      return this.getRandomFromArray(this.blueball);
+    },
+    getRandomRedBall() {
+      let targetArr = [...this.redball];
+      let redBallSelect = [];
+      while (redBallSelect.length < 6) {
+        let r = this.getRandomFromArray(targetArr);
+        let index = targetArr.indexOf(r);
+        redBallSelect.push(r);
+        targetArr.splice(index, 1);
+      }
+      return redBallSelect.sort((a,b)=>{return a-b});
+    },
+  },
+};
+</script>
+
+<style></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,89 +1,108 @@
 <template>
-	<view class="content">
-		<scroll-view overflow-x="false" overflow-y="true">
-			
-			<xt-panal-list :count="menuList.length">
-				<!-- #ifdef MP -->
-				<view v-for="(item) of menuList" slot="card{{index}}">
-					<text>{{item.title}}</text>
-				
-					
-				</view>
-				<!-- #endif -->
-				<!-- #ifdef H5 || APP-PLUS -->
-				<template v-for="(item,index) of menuList" v-slot:[`card${index}`]>
-					<view class="h">{{item.title}}</view>
-					<view class="li">
-						<view  class="menu-list">
-							
-							<view v-for="it of item.list">
-								<navigator :url="it.href" hover-class="navigator-hover">
-									<view class="menu-item" > 
-									<text class="tx"><l-icon :name="it.icon" size="30px" color="#a1a1a1"/></text>
-									<text class="tx">{{it.title}}</text>
-									</view>
-									
-								</navigator>
-								
-							</view>
-							
-						</view>
-						
-					</view>
-				</template>
-				<!-- #endif -->
-			</xt-panal-list>
-			
-		</scroll-view>
-	</view>
+  <view class="content">
+    <scroll-view overflow-x="false" overflow-y="true">
+      <xt-panal-list :count="menuList.length">
+        <!-- #ifdef MP -->
+        <view v-for="item of menuList" slot="card{{index}}">
+          <text>{{ item.title }}</text>
+        </view>
+        <!-- #endif -->
+        <!-- #ifdef H5 || APP-PLUS -->
+        <template v-for="(item, index) of menuList" v-slot:[`card${index}`]>
+          <view class="h">{{ item.title }}</view>
+          <view class="li">
+            <view class="menu-list">
+              <view v-for="it of item.list">
+                <navigator :url="it.href" hover-class="navigator-hover">
+                  <view class="menu-item">
+                    <text class="tx"
+                      ><l-icon :name="it.icon" size="30px" color="#a1a1a1"
+                    /></text>
+                    <text class="tx">{{ it.title }}</text>
+                  </view>
+                </navigator>
+              </view>
+            </view>
+          </view>
+        </template>
+        <!-- #endif -->
+      </xt-panal-list>
+    </scroll-view>
+  </view>
 </template>
 
 <script lang="ts">
-	import {ref} from "vue"
-	export default {
-		setup() {
-			const menuList:Object=[
-				{title:"计算",list:[{title:"显示器PPI计算",icon:"bi:display-fill",href:"/pages/ppi/ppi"},{title:"投影投射比",icon:"solar:projector-bold-duotone",href:"/pages/projectiveRatio/projectiveRatio"}]},
-				{title:"记账",list:[{title:"旅游消费统计",icon:"material-symbols:account-balance-wallet",href:"/pages/travelExpense/travelExpense"}]}
-			]
-			
-			return {menuList}
-		},
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-		console.log(11111,getApp().globalData)
-		},
-		methods: {
+import { ref } from "vue";
+export default {
+  setup() {
+    const menuList: Object = [
+      {
+        title: "计算",
+        list: [
+          {
+            title: "显示器PPI计算",
+            icon: "bi:display-fill",
+            href: "/pages/ppi/ppi",
+          },
+          {
+            title: "投影投射比",
+            icon: "solar:projector-bold-duotone",
+            href: "/pages/projectiveRatio/projectiveRatio",
+          },
+		  {
+		    title: "双色球计算",
+		    icon: "solar:projector-bold-duotone",
+		    href: "/pages/doubleColorBall/doubleColorBall",
+		  }
+        ],
+      },
+      {
+        title: "记账",
+        list: [
+          {
+            title: "旅游消费统计",
+            icon: "material-symbols:account-balance-wallet",
+            href: "/pages/travelExpense/travelExpense",
+          },
+        ],
+      },
+    ];
 
-		}
-	}
-
+    return { menuList };
+  },
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  onLoad() {
+    console.log(11111, getApp().globalData);
+  },
+  methods: {},
+};
 </script>
 
 <style lang="scss">
-
-.h{
-	font-size: 35px;
-	padding:10px 20px;
-}
-.menu-list{
-	display: flex;
-	gap:10px;
-	.menu-item{
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		padding:10px 10px;
-		.tx{
-			text-align: center;
-			font-size: 14px;
-			color: #a1a1a1;
-		}
-	}
+.h {
+  font-size: 35px;
+  padding: 10px 20px;
 }
 
+.menu-list {
+  display: flex;
+  gap: 10px;
+
+  .menu-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10px 10px;
+
+    .tx {
+      text-align: center;
+      font-size: 14px;
+      color: #a1a1a1;
+    }
+  }
+}
 </style>
